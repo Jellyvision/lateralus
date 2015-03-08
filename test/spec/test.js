fa5e5489ecef09263a5440570e56c7ee8dc9cecc
Jeremy Kahn <jeremyckahn@gmail.com>
2015-03-08 13:08:58 -0500

    Add some more Object structure tests.

--- test/spec/test.js
+++ test/spec/test.js
@@ -15,8 +15,20 @@ define([
   var assert = chai.assert;
 
   describe('Bootup', function () {
-    it('Should define Lateralus constructor', function () {
+    it('Defines Lateralus constructor', function () {
       assert.isFunction(Lateralus);
+    });
+
+    it('Defines Lateralus.Component', function () {
+      assert.isFunction(Lateralus.Component);
+    });
+
+    it('Defines Lateralus.Model', function () {
+      assert.isFunction(Lateralus.Model);
+    });
+
+    it('Defines Lateralus.Router', function () {
+      assert.isFunction(Lateralus.Router);
     });
   });
 
