0977c4464cb8e7020278c45737ec5621be51d2ba
Jeremy Kahn <jeremyckahn@gmail.com>
2015-03-08 13:53:18 -0500

    Add tests for lateralusEvents map.

--- test/spec/test.js
+++ test/spec/test.js
@@ -2,11 +2,13 @@
 define([
 
   'chai'
+  ,'underscore'
   ,'lateralus'
 
 ], function (
 
   chai
+  ,_
   ,Lateralus
 
 ) {
@@ -15,19 +17,19 @@ define([
   var assert = chai.assert;
 
   describe('Bootup', function () {
-    it('Defines Lateralus constructor', function () {
+    it('Framework defines Lateralus constructor', function () {
       assert.isFunction(Lateralus);
     });
 
-    it('Defines Lateralus.Component', function () {
+    it('Framework defines Lateralus.Component', function () {
       assert.isFunction(Lateralus.Component);
     });
 
-    it('Defines Lateralus.Model', function () {
+    it('Framework defines Lateralus.Model', function () {
       assert.isFunction(Lateralus.Model);
     });
 
-    it('Defines Lateralus.Router', function () {
+    it('Framework defines Lateralus.Router', function () {
       assert.isFunction(Lateralus.Router);
     });
   });
@@ -40,12 +42,80 @@ define([
     var el = document.createElement('div');
     var app = new App(el);
 
-    it('Has root element', function () {
+    it('App has root element', function () {
       assert.equal(app.el, el);
     });
 
-    it('Has jQuery reference to root element', function () {
+    it('App has jQuery reference to root element', function () {
       assert.equal(app.$el[0], el);
+    });
+  });
+
+  describe('lateralusEvents', function () {
+    var App = Lateralus.beget(function () {
+      Lateralus.apply(this, arguments);
+    });
+
+    var testWasCalled = false;
+
+    _.extend(App.prototype, {
+      lateralusEvents: {
+        test: function () {
+          testWasCalled = true;
+        }
+      }
+    });
+
+    var app = new App(document.createElement('div'));
+
+    it('Captures top-level events', function () {
+      app.emit('test');
+      assert.isTrue(testWasCalled);
+    });
+
+    testWasCalled = false;
+
+    it('Captures Lateralus.Model-level events', function () {
+      app.model.emit('test');
+      assert.isTrue(testWasCalled);
+    });
+
+    testWasCalled = false;
+
+    it('Captures Component-level events', function () {
+      var component = app.addComponent(Lateralus.Component);
+      component.emit('test');
+      assert.isTrue(testWasCalled);
+      component.dispose();
+    });
+
+    testWasCalled = false;
+
+    it('Captures Component.View-level events', function () {
+      var ExtendedComponent = Lateralus.Component.extend({
+        name: 'extended'
+        ,View: Lateralus.Component.View
+      });
+
+      var component = app.addComponent(ExtendedComponent);
+      component.view.emit('test');
+      assert.isTrue(testWasCalled);
+      component.dispose();
+    });
+
+    testWasCalled = false;
+
+    it('Captures Component.Model-level events', function () {
+      var ExtendedComponent = Lateralus.Component.extend({
+        name: 'extended'
+        ,View: Lateralus.Component.View
+        ,Model: Lateralus.Component.Model
+      });
+
+      var component = app.addComponent(ExtendedComponent);
+      component.view.model.emit('test');
+      assert.isTrue(testWasCalled);
+      component.dispose();
     });
   });
 
